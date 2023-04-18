4c5790b56f90fb0e128ea4d2607c9f0929f8c16b
RLyudmila <116638126+RLyudmila@users.noreply.github.com>
2023-04-18 20:40:49 +0300

    запись игрушек в файл

--- src/main/java/ToyStore/Program.java
+++ src/main/java/ToyStore/Program.java
@@ -10,16 +10,20 @@ public class Program {
         Toy toy2 = new Toy(1, "Машинка", 40);
         Toy toy3 = new Toy(2, "Робот", 3);
         Toy toy4 = new Toy(3, "Матрёшка", 65);
+        Toy toy5 = new Toy(4,"Игра",70);
 
         List<Toy> toys = new ArrayList<Toy>();
 
+            toys.add(toy5);
             toys.add(toy4);
             toys.add(toy3);
             toys.add(toy2);
             toys.add(toy1);
 
 
+
         ToysStore toyStore = new ToysStore(toys);
+        toyStore.saveToyForLottery();
         toyStore.saveToyForLottery();
         toyStore.saveToyForLottery();
         toyStore.saveToyForLottery();
